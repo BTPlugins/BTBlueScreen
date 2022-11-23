9ba898cb75f08b5330cf622fdc7822cc5780dd8b
blazethrower320 <blazethrower320@gmail.com>
2022-11-22 22:07:10 -0600

    Add project files.

---
+++ Commands/BlueScreenCommand.cs
@@ -0,0 +1,52 @@
+﻿using BlueScreen.Helpers;
+using Rocket.API;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueScreen.Commands
+{
+    public class BlueScreenCommand : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+        public string Name => "BlueScreen";
+
+        public string Help => "BlueScreen";
+
+        public string Syntax => "BlueScreen <Target>";
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string>() { "BTBlueScreen.BlueScreen" };
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            if(command.Length < 1)
+            {
+                TranslationHelper.SendMessageTranslation(player.CSteamID, "ProperUsage", "/BlueScreen <Target>");
+                return;
+            }
+            var target = UnturnedPlayer.FromName(command[0]);
+            if(target == null)
+            {
+                TranslationHelper.SendMessageTranslation(player.CSteamID, "TargetNotFound");
+                return;
+            }
+            if (BlueScreen.Instance.BlueScreens.Contains(target.CSteamID.m_SteamID))
+            {
+                // Remove BlueScreen
+                EffectManager.askEffectClearByID(18000, target.Player.channel.owner.transportConnection);
+                TranslationHelper.SendMessageTranslation(player.CSteamID, "BlueScreenRemoved", target.CharacterName);
+                BlueScreen.Instance.BlueScreens.Remove(target.CSteamID.m_SteamID);
+            }
+            EffectManager.sendUIEffect(18000, 180, target.Player.channel.owner.transportConnection ,true);
+            TranslationHelper.SendMessageTranslation(player.CSteamID, "BlueScreened", target.CharacterName);
+            BlueScreen.Instance.BlueScreens.Add(target.CSteamID.m_SteamID);
+        }
+    }
+}
---
+++ Translations.cs
@@ -0,0 +1,28 @@
+﻿using Rocket.API.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueScreen
+{
+    public partial class BlueScreen
+    {
+        public override TranslationList DefaultTranslations => new TranslationList
+        {
+            {
+                "ProperUsage", "[color=#FF0000]{{BTBlueScreen}} [/color] [color=#F3F3F3]Proper Usage |[/color] [color=#3E65FF]{0}[/color]"
+            },
+            {
+                "TargetNotFound", "[color=#FF0000]{{BTBlueScreen}} [/color][color=#F3F3F3]Target Not Found![/color]"
+            },
+            {
+                "BlueScreened", "[color=#FF0000]{{BTBlueScreen}} [/color][color=#F3F3F3]You have successfully BlueScreened[/color] [color=#3E65FF]{0}[/color]"
+            },
+            {
+                "BlueScreenRemoved", "[color=#FF0000]{{BTBlueScreen}} [/color][color=#3E65FF]{0}[/color] [color=#F3F3F3]has been Removed from the BlueScreen[/color]"
+            },
+        };
+    }
+}
